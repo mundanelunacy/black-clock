e0ba87f0c222c3da4538eefe94a8a28764c2be89
Satoshi Kawase <mundanelunacy@gmail.com>
2023-07-30 14:10:32 +0900

    readme

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 **Demo** [black-clock.web.app](https://black-clock.web.app/)
 
-A simple clock written with react. The clock is black and white and has a simple design. The clock is designed to be used as a screensaver on a computer or a clock on a phone and will keep the screen awake until it is shut down. The clock is designed to be used as a PWA so it can be installed on a phone or computer.
+A simple clock written with react with the following features. The clock is designed to be used as a screensaver on a computer or a clock on a phone
+
+-   Black and white and with a simple design
+-   Installable webapp that you can download onto your phone
+-   'Enable no sleep' button will keep the screen awake until the app is closed
+-   Extremely simple to customize with CSS
--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,23 @@ export const App: React.FC<{}> = () => {
     };
     const dateFontStyle: React.CSSProperties = { fontSize: "5vw" };
     const timeFontStyle: React.CSSProperties = { fontSize: "15vw" };
-    const noSleepFontStyle: React.CSSProperties = { color: noSleep.isEnabled ? "black" : "white" };
+    const noSleepButtonStyle: React.CSSProperties = {
+        color: noSleep.isEnabled ? "black" : "white",
+        border: noSleep.isEnabled ? "" : "1px solid white",
+        padding: "5px",
+        width: "130px",
+        textAlign: "center",
+        borderRadius: "5px",
+    };
+
     const onClick = () => (noSleep.isEnabled ? noSleep.disable() : noSleep.enable());
+
     return (
         <div style={containerStyle}>
             <div>
                 <div style={dateFontStyle}>{time.toLocaleDateString()}</div>
                 <div style={timeFontStyle}>{time.toLocaleTimeString()}</div>
-                <div style={noSleepFontStyle} onClick={onClick}>
+                <div style={noSleepButtonStyle} onClick={onClick}>
                     {noSleep.isEnabled ? "disable" : "enable"} no sleep
                 </div>
             </div>
